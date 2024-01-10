23bc11b9bce357fac84b1031e2830c97b014be02
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-10 18:18:03 +0200

    fixing reqs

--- config/python.py
+++ config/python.py
@@ -4,19 +4,20 @@ from typing import List
 console_scripts: List[str] = [
     "pynetflix=pynetflix.main:main",
 ]
-config_requires: List[str] = []
 dev_requires: List[str] = [
     "pypitools",
     "black",
 ]
-make_requires: List[str] = [
+config_requires: List[str] = [
     "pyclassifiers",
-    "pydmt",
 ]
 install_requires: List[str] = [
     "pytconf",
     "pylogconf",
     "browser_cookie3",
+]
+make_requires: List[str] = [
+    "pydmt",
 ]
 test_requires: List[str] = [
     "pytest",
