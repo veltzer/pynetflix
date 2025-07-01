b2e6b24d53917566b8bf3ebb2836ac7ba44d1931
Mark Veltzer <mark.veltzer@gmail.com>
2025-07-01 15:55:16 +0300

    better reqs

--- config/python.py
+++ config/python.py
@@ -1,27 +1,16 @@
 """ python deps for this project """
 
-scripts: dict[str,str] = {
-    "pynetflix": "pynetflix.main:main",
-}
+import config.shared
 
-config_requires: list[str] = [
-    "pyclassifiers",
-]
 install_requires: list[str] = [
     "pytconf",
     "pylogconf",
     "browser_cookie3",
 ]
-build_requires: list[str] = [
-    "hatch",
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
-]
-test_requires: list[str] = [
-    "pytest",
-    "pylint",
-    "mypy",
-    "ruff",
-]
-requires = config_requires + install_requires + build_requires + test_requires
+build_requires: list[str] = config.shared.PBUILD
+test_requires: list[str] = config.shared.PTEST
+requires = install_requires + build_requires + test_requires
+
+scripts: dict[str,str] = {
+    "pynetflix": "pynetflix.main:main",
+}
